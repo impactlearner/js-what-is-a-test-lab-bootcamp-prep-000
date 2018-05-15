2a4e3160a371d2352812e1064318566cd8946e8b
impactlearner <time.darden@gmail.com>
2018-05-15 21:59:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // write your code below!
 var name = "Susan"
-var height = 74
-var message = ` ${name}is ${'is less than 40'} inches tall`
+var height = 'less than 40'
+var message = ` ${name}is ${'less than 40'} inches tall`
 
 module.exports = {name, height, message}
